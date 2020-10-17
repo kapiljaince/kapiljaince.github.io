a4dd3314fef563277c8dd1b981620adc4e286aff
Priyank Solanki <priyanksolanki30197@gmail.com>
2020-10-17 22:36:19 +0530

    CHanges as per client given

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,9 +114,9 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
-    loop: true,
+    loop: false,
     responsive: {
       0: {
         items: 1
@@ -124,9 +124,9 @@
       768: {
         items: 2
       },
-      900: {
-        items: 3
-      }
+      // 900: {
+      //   items: 3
+      // }
     }
   });
 
